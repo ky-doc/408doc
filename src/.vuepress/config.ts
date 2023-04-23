79c90a71bc1e58b59d5d59728c2e35401a1c84bd
408doc <doc_408@163.com>
2023-04-23 19:03:43 +0800

    update

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import theme from "./theme.js";
 import { searchProPlugin } from "vuepress-plugin-search-pro";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/408doc/",
 
   lang: "zh-CN",
   title: "研小布「考研资料站」",
